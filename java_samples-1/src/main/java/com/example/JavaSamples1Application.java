3e006769a891d1f8cc88f6c56309fc3c84dccbac
imagawa-yuuki0827 <imagawa.yuuki808@mail.kyutech.jp>
2022-05-29 06:29:41 +0900

    最初のコミットです

---
+++ java_samples-1/src/main/java/com/example/JavaSamples1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSamples1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSamples1Application.class, args);
+	}
+
+}
